748f1c000a0443e78b38c935c5944ef12e2616f3
Jason Ching <jching83@gmail.com>
2022-05-10 12:38:33 -0500

    Add files via upload

---
+++ diamonPattern.cpp
@@ -0,0 +1,33 @@
+/*
+Diamond Pattern
+Write a program that displays the following pattern:
+	*
+   ***
+  *****
+ *******
+  *****
+   ***
+    *
+	* 
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+	//outputs diamon pattern
+	cout << "   *   " << endl;
+	cout << "  ***  " << endl;
+	cout << " ***** " << endl;
+	cout << "*******" << endl;
+	cout << " ***** " << endl;
+	cout << "  ***  " << endl;
+	cout << "   *   " << endl;
+
+	return 0;
+
+}
+
