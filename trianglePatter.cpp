0b4d1f25b3e5f1a51e2c74c515fb99630c1ec457
Jason Ching <jching83@gmail.com>
2022-05-10 12:31:56 -0500

    Add files via upload

---
+++ trianglePatter.cpp
@@ -0,0 +1,24 @@
+/*
+Triangle Pattern
+Write a program that displays the following pattern on the screen:
+   *
+  ***
+ *****
+*******
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	//outputs a triangle pattern
+	cout << "   *   " << endl;
+	cout << "  ***  " << endl;
+	cout << " ***** " << endl;
+	cout << "*******" << endl;
+
+	return 0;
+}
